c3965892291f73b599eea823690482f54affada6
Chris <rolletchris1@gmail.com>
2024-09-15 12:43:04 -0700

    Add CORS middleware for enabling cross-origin resource sharing

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -4,11 +4,18 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Enable CORS for all routes
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	// Simple hello world test
 	app.Get("/", func(c *fiber.Ctx) error {
