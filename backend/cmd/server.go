d6d8e7d7754ad63feb271ae35fad42b370b089c8
Chris <rolletchris1@gmail.com>
2024-09-20 19:17:43 -0700

    Refactor server configuration to use environment variables and initialize database connection

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/joho/godotenv"
+	"github.com/sandbox-science/online-learning-platform/configs"
+	"github.com/sandbox-science/online-learning-platform/internal/entity"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{})
+
+	// Load the environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Set the listen address
+	listenAddr := os.Getenv("HOST_ADDR")
+	if len(listenAddr) == 0 {
+		listenAddr = ":8080"
+	}
+
+	// Initialize the database configuration
+	conf := entity.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
 
 	// Enable CORS for all routes
 	app.Use(cors.New(cors.Config{
@@ -25,7 +52,14 @@ func main() {
 	// Add healthcheck middleware for /livez and /readyz
 	app.Use(healthcheck.New(healthcheck.Config{}))
 
-	if err := app.Listen(":4000"); err != nil {
+	// Initialize the database
+	db, err := configs.InitDB(conf)
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	fmt.Printf("%v", db) // remove this when db is used in the core service
+
+	if err := app.Listen(listenAddr); err != nil {
 		log.Fatal(err)
 	}
 }
