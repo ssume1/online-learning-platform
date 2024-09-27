ac62a6889809fd92124a938eaca3b985f0cc12ec
zachspang <spangzach@gmail.com>
2024-09-27 13:35:25 -0700

    Fix navbar alignment and add account button

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,6 +6,9 @@ import { FaSchool,FaBars,FaTimes } from "react-icons/fa";
 function Navbar() {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
+    /*Once user accounts are implemented on the backend add a useEffect to 
+    fetch if the user is logged in and update the variable below */
+    const [isLoggedIn, setisLoggedIn] = useState(false);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
@@ -31,10 +34,10 @@ function Navbar() {
     return (
         <>
             <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         <FaSchool />&nbsp;OLP
-                    </Link>
+                </Link>
+                <div className="navbar-container">
                     <div className="menu-icon" onClick={handleClick}>
                         {click ? <FaTimes style={{color: "white"}}/> : <FaBars style={{color: "white"}} />}
                     </div>
@@ -49,24 +52,37 @@ function Navbar() {
                                 Courses
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/login" className="nav-links-mobile" onClick={closeMobileMenu}>
-                                Log In
+                        {/*If there is a user logged in replace the sign up and login buttons with the account button*/}
+                        {!isLoggedIn ? 
+                            <li className="nav-item">
+                                <Link to="/login" className="nav-links-mobile" onClick={closeMobileMenu}>
+                                    Log In
+                                </Link>
+                            </li>: 
+                            <Link to="/account" className="nav-links-mobile" onClick={closeMobileMenu}>
+                                Account
                             </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/signup" className="nav-links-mobile" onClick={closeMobileMenu}>
-                                Sign Up
-                            </Link>
-                        </li>
+                        }
+                        {!isLoggedIn ? <li className="nav-item">
+                                <Link to="/signup" className="nav-links-mobile" onClick={closeMobileMenu}>
+                                    Sign Up
+                                </Link>
+                        </li> : null}
                     </ul>
-                    <Link to="/login" className="btn-mobile"> 
-                        {button && <Button buttonStyle="btn--outline">Log In</Button>}
-                    </Link>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to="/signup">
-                        {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
-                    </Link>
+                    
+                    {!isLoggedIn ? <div style={{display: "flex", justifyContent: "end"}}>
+                        <Link to="/login" className="btn-mobile"> 
+                            {button && <Button buttonStyle="btn--outline">Log In</Button>}
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to="/signup">
+                            {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
+                        </Link>
+                    </div> : 
+                        <Link to="/Account" className="btn-mobile"> 
+                        {button && <Button buttonStyle="btn--outline">Account</Button>}
+                        </Link>
+                    }
                     
                 </div>
             </nav>
