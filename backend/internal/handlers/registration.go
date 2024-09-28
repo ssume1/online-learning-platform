30782018a7cca87d8398f15f5edc1fd3f528c41d
Chris <rolletchris1@gmail.com>
2024-09-28 15:54:48 -0700

    Add registration handler to create a new user and store it in the database

---
+++ backend/internal/handlers/registration.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/sandbox-science/online-learning-platform/configs/database"
+	"github.com/sandbox-science/online-learning-platform/internal/entity"
+	"github.com/sandbox-science/online-learning-platform/internal/utils"
+)
+
+// Register creates a new user and add it to the database.
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Invalid input"})
+	}
+
+	// Validate input
+	if data["password"] != data["confirm_password"] {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Passwords do not match"})
+	}
+
+	user := entity.Account{
+		Username: data["username"],
+		Email:    data["email"],
+		Password: data["password"],
+	}
+
+	// Hash password
+	if err := utils.HashPassword(&user); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Couldn't hash password"})
+	}
+
+	// Add user to database
+	if err := database.DB.Create(&user).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "User registered successfully",
+	})
+}
