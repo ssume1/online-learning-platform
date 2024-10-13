3c6ae261b316d422fc5dec95c2a417643b1e98c7
Chris <rolletchris1@gmail.com>
2024-10-12 18:38:11 -0700

    Remove ID field from Account struct in entity package

--- backend/internal/entity/account.go
+++ backend/internal/entity/account.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 type Account struct {
 	gorm.Model
-	ID       uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Username string `gorm:"size:255;not null;" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"-"`
 	Email    string `gorm:"size:100;not null;unique" json:"email"`
